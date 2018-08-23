4905b8d622bd910582ec7e4aad8e86366a2ed7ae
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-23 21:08:36 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  resources :artists, only: [:show, :index] do
+    resources :songs, only: [:show, :index]
+  end
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+
+  root 'songs#index'
 end
