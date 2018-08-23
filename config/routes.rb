1281d08fd94f5087a576d44387fdb317f26f4755
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-23 20:52:39 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :artists
   resources :songs
 
-  get '/artists/:id/songs'
-  get '/artists/:id/songs/:song_id'
+  get '/artists/:id/songs', to: 'artists#songs_index'
+  get '/artists/:id/songs/:song_id', to: 'artists#song'
 end
