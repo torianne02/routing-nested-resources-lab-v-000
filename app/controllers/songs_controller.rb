0c068b2171cd6efa89697f4b6ba27f82c531866a
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-23 20:59:22 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).posts
+      @songs = Artist.find(params[:artist_id]).songs
     else
       @songs = Song.all
     end
