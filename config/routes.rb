9014b457753aa940c7b67f5201cb3c0eec67b7f7
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-23 21:06:36 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :artists, only: [:show, :index] do
     resources :songs, only: [:show, :index]
   end
-
   resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'songs#index'
